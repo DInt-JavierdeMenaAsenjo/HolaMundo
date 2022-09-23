4ac57c4440b9642a51cba45c8eb239fd53a93a74
JAVIER DE MENA ASENJO <javdemase@alu.edu.gva.es>
2022-09-23 12:48:15 +0200

    Creada la ventana Hola Mundo version 1

---
+++ prueba/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace prueba
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (nombreTextBox.Text.Equals("")) nombreTextBlock.Text = "Hola Mundo!";
+            else nombreTextBlock.Text = "Hola " + nombreTextBox.Text + "!";
+        }
+    }
+}
